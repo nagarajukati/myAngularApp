a6cb67b7a6f2eb16f9b9516e363341991d61bdc2
acenagaraju <nagaraju@aceable.com>
2020-12-25 11:40:01 +0530

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myAngularApp'`, () => {
+  it(`should have as title 'Nagaraju'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myAngularApp');
+    expect(app.title).toEqual('Nagaraju');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('myAngularApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Nagaraju app is running!');
   });
 });
